aa71d57ce4b8cdec8861fb4794d313553762611d
Wang Chuanbin <Wang.Chuanbin@sloong.com>
2015-10-08 17:57:11 +0800

    Add first version.

---
+++ WebDownload/WebDownload/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace WebDownload
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxStartUrl = new System.Windows.Forms.TextBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxStartUrl
+            // 
+            this.textBoxStartUrl.Location = new System.Drawing.Point(60, 12);
+            this.textBoxStartUrl.Name = "textBoxStartUrl";
+            this.textBoxStartUrl.Size = new System.Drawing.Size(325, 21);
+            this.textBoxStartUrl.TabIndex = 0;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(473, 12);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(97, 23);
+            this.buttonStart.TabIndex = 1;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(745, 410);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.textBoxStartUrl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxStartUrl;
+        private System.Windows.Forms.Button buttonStart;
+    }
+}
+
---
+++ WebDownload/WebDownload/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebDownload
+{
+    public partial class Form1 : Form
+    {
+
+        List<string> workList = new List<string>();
+        List<string> imageList = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+
+                WebClient MyWebClient = new WebClient();
+
+
+                MyWebClient.Credentials = CredentialCache.DefaultCredentials;//获取或设置用于向Internet资源的请求进行身份验证的网络凭据
+
+                Byte[] pageData = MyWebClient.DownloadData("http://desk.3987.com/"); //从指定网站下载数据
+
+                string pageHtml = Encoding.Default.GetString(pageData);  //如果获取网站页面采用的是GB2312，则使用这句            
+
+                //string pageHtml = Encoding.UTF8.GetString(pageData); //如果获取网站页面采用的是UTF-8，则使用这句
+                pageHtml = pageHtml.Replace("<", "\n<");
+                string[] listtemp = Regex.Split(pageHtml, "\n", RegexOptions.IgnoreCase);
+                //List<string> list = new List<string>(listtemp);
+                List<string> list = new List<string>();
+                foreach (var item in listtemp)
+                {
+                    var temp = item.Replace("\t", "");
+                    temp = temp.Trim();
+                    if (temp.Length > 5)
+                        list.Add(temp);
+                }
+                List<string> imglist = new List<string>();
+                List<string> linklist = new List<string>();
+                foreach(var item in list)
+                {
+                    if (-1 != item.IndexOf("<img"))
+                        imglist.Add(item);
+
+                    if (-1 != item.IndexOf("<a"))
+                        linklist.Add(item);
+
+
+                }
+
+                // 开始处理imagelist
+                foreach( var item in imglist)
+                {
+                    int start = item.IndexOf("src=");
+                    start = item.IndexOf("\"", start);
+                    int end = item.IndexOf("\"", start+1);
+                    string url = item.Substring(start+1, end - start-1);
+                    string filename = url.Substring(7);
+                    string filepath = "D:\\" + filename;
+                    filepath = filepath.Replace("/", "_");
+                    WebClient mywebclient = new WebClient();
+                    mywebclient.DownloadFile(url, filepath);
+                }
+
+                using (StreamWriter sw = new StreamWriter("D:\\ouput.html"))//将获取的内容写入文本
+                {
+
+                    //sw.Write(pageHtml);
+
+                }
+            }
+
+            catch (WebException webEx)
+            {
+
+                Console.WriteLine(webEx.Message.ToString());
+
+            }
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            workList.Clear();
+            workList.Add(textBoxStartUrl.Text);
+        }
+    }
+}
